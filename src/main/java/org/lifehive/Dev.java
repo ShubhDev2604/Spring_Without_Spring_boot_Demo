a4994ed6b116fbb09ae5b13c34c188e619b8f657
Shubh Tandon <shubhtandon802@gmail.com>
2025-11-24 13:38:44 +0530

    Added constructor and setter injection

--- src/main/java/org/lifehive/Dev.java
+++ src/main/java/org/lifehive/Dev.java
@@ -2,7 +2,31 @@ package org.lifehive;
 
 public class Dev {
     static private int objCount = 0;
+
+    public int getObjNo() {
+        return objNo;
+    }
+
+    public void setObjNo(int objNo) {
+        this.objNo = objNo;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public Dev(int objNo, Laptop laptop) {
+        this.objNo = objNo;
+        this.laptop = laptop;
+        System.out.println("Hii Baby... I am Object no." + objNo);
+    }
+
     private int objNo;
+    private Laptop laptop;
     Dev() {
         objCount++;
         objNo = objCount;
@@ -10,5 +34,6 @@ public class Dev {
     }
     public void build() {
         System.out.println("Hii Dev...I am using object no. " + objNo);
+        laptop.start();
     }
 }
